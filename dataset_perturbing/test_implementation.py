a42c1575fd12201cd8255a3262d6001bf4be877c
kearapolovick <uzy2ws@virginia.edu>
2023-04-13 20:18:58 -0400

    /

--- dataset_perturbing/test_implementation.py
+++ dataset_perturbing/test_implementation.py
@@ -9,7 +9,6 @@ def adjust_solution(new_dataset, old_dataset, old_solution):
 
     old_descriptor = {[1] for i in old_solution}
     print(old_descriptor)
-    print(old_data[1:])
     for row in old_data[1:]:
         # TODO: implement the rest of this section
         i = row
