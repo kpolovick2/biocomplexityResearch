60fc44972a045249c0f193d175c7caa7661a3017
uhhJoyz <wbradford@renaissanceschool.org>
2023-05-05 21:52:28 -0400

    added further memory optimizations, tested, and noted findings

--- polynomial_add/poly_add_timer.py
+++ polynomial_add/poly_add_timer.py
@@ -2,7 +2,7 @@ import timeit
 import one_cluster_ilp as ILP
 import dataset_perturbing.perturb_utilities as ptu
 
-test_count = 1200
+test_count = 12
 
 orig_file = "1000diagonal.txt"
 file = "1000diagonal_1.txt"
@@ -12,7 +12,7 @@ desc = ptu.string_descriptor_to_array(ILP.ILP_one_cluster(f"../test_txt_files/{o
 ilp_time = timeit.timeit(f"a.ILP_linear('../test_txt_files/{file}')",  setup="import ILP_linear as a",
                          number=test_count)
 revised_time = timeit.timeit(f"print(p.update_descriptor_multi_item('../test_txt_files/{orig_file}',"
-                             f" {desc}, '../test_txt_files/{file}'))", setup="import poly_add as p", number=test_count)
+                             f" {desc}, '../test_txt_files/{file}'))", setup="import poly_add_mem as p", number=test_count)
 
 print(f"The ILP solved this problem in {ilp_time/test_count} seconds per execution.")
 print(f"The polynomial time add algorithm solved this problem in {revised_time/test_count} seconds per execution.")
