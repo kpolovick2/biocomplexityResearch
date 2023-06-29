99b3e30c7cd532bc3667cdb93c459b261c0c1c3d
uhhJoyz <wbradford@renaissanceschool.org>
2023-06-29 10:03:29 -0400

    added parallel heuristic

--- polynomial_add/parallel_add.py
+++ polynomial_add/parallel_add.py
@@ -23,9 +23,8 @@ def mut_vec_diff(v1, v2):
 #     thread.start_new_thread(mut_vec_diff(v1[:len(v1) // 2], v2[:len(v2) // 2]))
 #     return
 
-def add_tags_internal(desc, dataset, tag_added, items):
+def add_tags_internal(dataset, desc, tag_added, items):
     vec_desc = np.zeros(shape=(len(desc) + 1, len(dataset)), dtype=np.ushort)
-
     for (i, t) in enumerate(desc):
         for (j, item) in enumerate(dataset[:, t]):
             vec_desc[i, j] = item
@@ -50,25 +49,44 @@ def add_tags_internal(desc, dataset, tag_added, items):
     for idx in tags_removed:
         desc[idx] = -1
 
-    new_desc = np.array([i for i in desc if i != -1])
+    new_desc = [i for i in desc if i != 4294967295]
+    new_desc.append(t)
+    new_desc = np.array(new_desc)
 
     if len(tags_removed) > 0:
         return new_desc
-
     return desc
 
 
 def add_tags(file, desc, new_file):
     data = parse_dataset(file)
+    # while len(data[0]) < len(data[1]):
+    #     data[0].append(0)
     data = data[:-1] if len(data[-1]) == 0 else data
     data = np.array(data, dtype=np.ushort)
     new_data = parse_dataset(new_file)
+    # while len(new_data[0]) < len(new_data[1]):
+    #     new_data[0].append(0)
     new_data = new_data[:-1] if len(new_data[-1]) == 0 else new_data
     new_data = np.array(new_data, dtype=np.ushort)
     tag_added = -1
     items = []
+    for (i, item) in enumerate(data):
+        # for each tag value in the item
+        for (j, t) in enumerate(item):
+            # if the two datasets do not match
+            if t != new_data[i][j]:
+                # set tag_added to the tag
+                tag_added = j - 1
+                # add i to the list of items
+                items.append(i)
+
+    if tag_added == -1:
+        return desc
+    return add_tags_internal(data, np.array(desc, dtype=np.uint), tag_added, np.array(items, dtype=np.uint))
 
 
 
 if __name__ == "__main__":
-    add_tags_internal(random.sample(range(4), random.randint(1, 4)), np.array([[random.randint(0, 1) for j in range(4)] for i in range(4)], dtype=np.ushort), 2, [1, 2])
+    print(add_tags("../test_txt_files/1000diagonal.txt", list(range(1,3)), "../test_txt_files/1000diagonal_1.txt"))
+    # add_tags_internal(random.sample(range(4), random.randint(1, 4)), np.array([[random.randint(0, 1) for j in range(4)] for i in range(4)], dtype=np.ushort), 2, [1, 2])
--- polynomial_add/poly_add_timer.py
+++ polynomial_add/poly_add_timer.py
@@ -9,11 +9,13 @@ file = "2000diagonal_1.txt"
 
 desc = ptu.string_descriptor_to_array(ILP.ILP_one_cluster(f"../test_txt_files/{orig_file}"))[0]
 
-ilp_time = timeit.timeit(f"a.ILP_linear('../test_txt_files/{file}')",  setup="import ILP_linear as a",
-                         number=test_count)
+# ilp_time = timeit.timeit(f"a.ILP_linear('../test_txt_files/{file}')",  setup="import ILP_linear as a",
+#                          number=test_count)
 revised_time = timeit.timeit(f"print(p.update_descriptor_multi_item('../test_txt_files/{orig_file}',"
-                             f" {desc}, '../test_txt_files/{file}'))", setup="import network_flow_formulation as p", number=test_count)
+                             f" {desc}, '../test_txt_files/{file}'))", setup="import poly_add as p", number=test_count)
+np_time = timeit.timeit(f"print(p.add_tags('../test_txt_files/{orig_file}',"
+                             f" {desc}, '../test_txt_files/{file}'))", setup="import parallel_add as p", number=test_count)
 
-print(f"The ILP solved this problem in {ilp_time/test_count} seconds per execution.")
+print(f"The ILP solved this problem in {np_time/test_count} seconds per execution.")
 print(f"The polynomial time add algorithm solved this problem in {revised_time/test_count} seconds per execution.")
-print(f"The polynomial algorithm was {ilp_time/revised_time} times faster.")
+print(f"The polynomial algorithm was {np_time/revised_time} times faster.")
